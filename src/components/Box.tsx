26f97921861db7e8471c8aa0a888d26f524e9023
eswar-7116 <eswardudi06@gmail.com>
2025-12-29 19:31:24 +0530

    fix: fix color issues

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -5,12 +5,10 @@ type BoxProps = {
 } & HTMLProps<HTMLDivElement>;
 
 export default function Box({ value, ...props }: BoxProps) {
-  const color = value === "X" ? "emerald-400" : "cyan-500";
-
   return (
     <div
       {...props}
-      className={`flex items-center justify-center bg-zinc-700 size-30 m-2 rounded-xl hover:bg-zinc-600 cursor-pointer text-5xl font-bold text-${color} text-shadow-lg text-shadow-${color}`}
+      className={`flex items-center justify-center bg-zinc-700 size-30 m-2 rounded-xl hover:bg-zinc-600 cursor-pointer text-5xl ${value === "X" ? "text-emerald-400" : "text-cyan-500"} text-shadow-md ${value === "X" ? "text-shadow-emerald-400" : "text-shadow-cyan-500"}`}
     >
       {value}
     </div>
