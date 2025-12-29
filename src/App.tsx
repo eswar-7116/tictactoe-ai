90cc71570f7f2a9a749004d3f036d94a1f0f7de8
eswar-7116 <eswardudi06@gmail.com>
2025-12-29 21:39:49 +0530

    feat: implement bot using minimax algo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "./components/Box";
+import bestMove, { isTermination } from "./util/bot";
 
 export default function App() {
   const [board, setBoard] = useState(new Array<string>(9).fill(""));
+  const [gameOver, setGameOver] = useState(false);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check if game ends
+    const [terminated, winner] = isTermination(board);
+    if (!terminated) return;
+
+    setGameOver(true);
+    if (winner === "") setMessage("It's a tie!");
+    else if (winner === "X") setMessage("Congratulations! You won!!");
+    else setMessage("Uh oh! You lost!");
+  }, [board]);
+
+  const play = (idx: number) => {
+    // Check if game is over
+    if (gameOver) return;
+
+    setBoard((prev) => {
+      const next = [...prev];
+
+      // Human's move
+      next[idx] = !prev[idx] || prev[idx] === "O" ? "X" : "O";
+
+      // Bot's move
+      const botIdx = bestMove(next);
+      if (botIdx !== -1) next[botIdx] = "O";
+
+      return next;
+    });
+  };
+
+  const reset = () => {
+    setBoard(new Array<string>(9).fill(""));
+    setGameOver(false);
+  }
 
   return (
     <div className="flex flex-col justify-center items-center h-full">
-      <header className="m-10 text-6xl">Tic-Tac-Toe</header>
+      <header className="mb-10 text-6xl">Tic-Tac-Toe</header>
+      <p className="text-2xl font-medium">{message}</p>
       <main className="m-3">
         {[0, 3, 6].map((start) => (
           <div
@@ -16,22 +54,29 @@ export default function App() {
             {[0, 1, 2].map((i) => {
               const idx = start + i;
               return (
-                <Box
-                  value={board[idx]}
-                  key={idx}
-                  onClick={() => {
-                    setBoard((prev) => {
-                      const next = [...prev];
-                      next[idx] = (!prev[idx] || prev[idx] === 'O') ? "X" : "O";
-                      return next;
-                    });
-                  }}
-                />
+                <Box value={board[idx]} key={idx} onClick={() => play(idx)} />
               );
             })}
           </div>
         ))}
       </main>
+      <div className="rounded-full bg-red-800 mt-6 flex items-center justify-center p-4 will-change-transform transition-transform hover:scale-110 cursor-pointer" onClick={() => reset()}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="scale-150"
+        >
+          <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+          <path d="M3 3v5h5" />
+        </svg>
+      </div>
     </div>
   );
 }
---
+++ src/util/bot.ts
@@ -0,0 +1,73 @@
+export function isTermination(state: string[]) {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+
+    // Check if any player won
+    for (const [a, b, c] of winPatterns) {
+        if (state[a] !== "" && state[a] === state[b] && state[b] === state[c]) {
+            return [true, state[a]];
+        }
+    }
+
+    // Check if it is a tie
+    const isTie = state.every(v => v !== "");
+
+    return [isTie, ""];
+}
+
+function minimax(state: string[], isMaximizing: boolean) {
+    // Base condition: check if game is over and return score
+    const [terminated, winner] = isTermination(state);
+    if (terminated) {
+        if (winner === "X") return 1;
+        if (winner === "O") return -1;
+        return 0;
+    }
+
+    // Play as 'X'
+    if (isMaximizing) {
+        let best = -Infinity;
+        for (let i=0; i<9; i++) {
+            if (state[i] === "") {
+                state[i] = "X";
+                best = Math.max(best, minimax(state, false))
+                state[i] = "";
+            }
+        }
+        return best;
+    }
+
+    // Play as 'O'
+    let best = Infinity;
+    for (let i=0; i<9; i++) {
+        if (state[i] === "") {
+            state[i] = "O";
+            best = Math.min(best, minimax(state, true));
+            state[i] = "";
+        }
+    }
+    return best;
+}
+
+export default function bestMove(state: string[]) {
+    // Play as 'O'
+    let bestScore = Infinity;
+    let ret = -1;
+    for (let i=0; i<9; i++) {
+        if (state[i] === "") {
+            state[i] = "O";
+            
+            let score = minimax(state, true);
+            if (bestScore > score) {
+                bestScore = score;
+                ret = i;
+            }
+
+            state[i] = "";
+        }
+    }
+    return ret;
+}
