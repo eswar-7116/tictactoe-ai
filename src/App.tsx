115c93b8956562bd0efbc2e0cf6ba83a8800ecc3
eswar-7116 <eswardudi06@gmail.com>
2025-12-29 18:08:55 +0530

    develop frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import Box from "./components/Box";
+
 export default function App() {
+  const [board, setBoard] = useState(new Array<string>(9).fill(""));
+
   return (
-    <>
-      <div>TicTacToe</div>
-    </>
+    <div className="flex flex-col justify-center items-center h-full">
+      <header className="m-10 text-6xl">Tic-Tac-Toe</header>
+      <main className="m-3">
+        {[0, 3, 6].map((start) => (
+          <div
+            className="flex flex-row items-center justify-center"
+            key={start}
+          >
+            {[0, 1, 2].map((i) => {
+              const idx = start + i;
+              return (
+                <Box
+                  value={board[idx]}
+                  key={idx}
+                  onClick={() => {
+                    setBoard((prev) => {
+                      const next = [...prev];
+                      next[idx] = (!prev[idx] || prev[idx] === 'O') ? "X" : "O";
+                      return next;
+                    });
+                  }}
+                />
+              );
+            })}
+          </div>
+        ))}
+      </main>
+    </div>
   );
 }
